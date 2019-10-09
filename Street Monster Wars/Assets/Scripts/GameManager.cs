8010dcc5cbb95e5ddbfbe37119b9ced4f7adfb05
Cam Wang <wang348@mail.chapman.edu>
2019-10-08 20:26:31 -0700

    Added example monster spwaning with button

--- Street Monster Wars/Assets/Scripts/GameManager.cs
+++ Street Monster Wars/Assets/Scripts/GameManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int playerOneCurrency = 0;
+    public int cookieCost;
+    public GameObject spawnMonster;
+    private float timeToPay = 1;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= timeToPay)
+        {
+            timer = 0;
+            playerOneCurrency += 20;
+        }
+    }
+    public void spawnCookieMonster()
+    {
+        if (playerOneCurrency >= cookieCost)
+        {
+            playerOneCurrency -= cookieCost;
+            Instantiate(spawnMonster, new Vector3(-7, 0, 0), Quaternion.identity);
+        }
     }
 }
---
+++ Street Monster Wars/Assets/Scripts/badMonsterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class badMonsterController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + speed, this.transform.position.y, this.transform.position.z);
+        if(this.transform.position.x > 12)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

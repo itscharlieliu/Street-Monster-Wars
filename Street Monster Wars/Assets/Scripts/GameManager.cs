d455598ed0b0def1f24f4cf72ac14f0f05a2ae1f
Cam Wang <wang348@mail.chapman.edu>
2019-10-10 09:43:38 -0700

    Made buttons working

--- Street Monster Wars/Assets/Scripts/GameManager.cs
+++ Street Monster Wars/Assets/Scripts/GameManager.cs
@@ -25,12 +25,76 @@ public class GameManager : MonoBehaviour
             playerOneCurrency += 20;
         }
     }
-    public void spawnCookieMonster()
+    private void spawnBirdMonster(bool movingRight)
     {
+        GameObject temp;
         if (playerOneCurrency >= cookieCost)
         {
             playerOneCurrency -= cookieCost;
-            Instantiate(spawnMonster, new Vector3(-7, 0, 0), Quaternion.identity);
+            if (movingRight)
+            {
+                temp = Instantiate(spawnMonster, new Vector3(-7, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                temp = Instantiate(spawnMonster, new Vector3(7, 0, 0), Quaternion.identity);
+            }
+            temp.GetComponent<SpriteRenderer>().color = Color.yellow;
+            temp.GetComponent<badMonsterController>().movingRight = movingRight;
+        }
+    }
+    private void spawnTrashMonster(bool movingRight)
+    {
+        GameObject temp;
+        if (playerOneCurrency >= cookieCost)
+        {
+            playerOneCurrency -= cookieCost;
+            if(movingRight)
+            {
+                temp = Instantiate(spawnMonster, new Vector3(-7, 0, 0), Quaternion.identity);
+            }
+            else
+            {
+                temp = Instantiate(spawnMonster, new Vector3(7, 0, 0), Quaternion.identity);
+            }
+            temp.GetComponent<SpriteRenderer>().color = Color.green;
+            temp.GetComponent<badMonsterController>().movingRight = movingRight;
+        }
+    }
+    public void spawnMonsterSelectorP1(int select)
+    {
+        switch(select)
+        {
+            case 0: //oscar trash
+                spawnTrashMonster(true);
+                break;
+            case 1: //big bird
+                spawnBirdMonster(true);
+                break;
+            case 2: //count dracula
+                break;
+            case 3: //bert
+                break;
+            default:
+                break;
+        }
+    }
+    public void spawnMonsterSelectorP2(int select)
+    {
+        switch (select)
+        {
+            case 0: //oscar trash
+                spawnTrashMonster(false);
+                break;
+            case 1: //big bird
+                spawnBirdMonster(false);
+                break;
+            case 2: //count dracula
+                break;
+            case 3: //bert
+                break;
+            default:
+                break;
         }
     }
 }
--- Street Monster Wars/Assets/Scripts/badMonsterController.cs
+++ Street Monster Wars/Assets/Scripts/badMonsterController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class badMonsterController : MonoBehaviour
 {
+    public bool movingRight;
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -14,8 +15,17 @@ public class badMonsterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(this.transform.position.x + speed, this.transform.position.y, this.transform.position.z);
-        if(this.transform.position.x > 12)
+        if(movingRight)
+        {
+
+            this.transform.position = new Vector3(this.transform.position.x + speed, this.transform.position.y, this.transform.position.z);
+        }
+        else
+        {
+
+            this.transform.position = new Vector3(this.transform.position.x - speed, this.transform.position.y, this.transform.position.z);
+        }
+        if(this.transform.position.x > 12 || this.transform.position.x < -12)
         {
             Destroy(this.gameObject);
         }

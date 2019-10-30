3364bb64b89205835034974dc3fbe0ccb7cbcf57
S3TO <ninja00000001@gmail.com>
2019-10-30 16:58:40 -0700

    Added cooldowns and base art

--- Street Monster Wars/Assets/Scripts/GameManager.cs
+++ Street Monster Wars/Assets/Scripts/GameManager.cs
@@ -22,8 +22,9 @@ public class GameManager : MonoBehaviour
     private float timeToPay = 1;
     private float timer;
 
-
-
+    //Cooldown stuff
+    private bool canSpawnBigBird1 = true, canSpawnBigBird2 = true, canSpawnOscar1 = true, canSpawnOscar2 = true, canSpawnBert1 = true, canSpawnBert2 = true, canSpawnDracula1 = true, canSpawnDracula2 = true;
+    private GameObject bbOverlay1, bbOverlay2, oOverlay1,oOverlay2,bOverlay1,bOverlay2,dOverlay1,dOverlay2;
     //Prefabs
     public GameObject bigBird,dracula,bert,oscar;
 
@@ -48,6 +49,24 @@ public class GameManager : MonoBehaviour
         p2RecentPress = false;
         p1ResearchTree.SetActive(playerOneResearching);
         p2ResearchTree.SetActive(playerTwoResearching);
+
+        bbOverlay1 = GameObject.Find("BigBirdOverlay1");
+        bbOverlay1.SetActive(false);
+        oOverlay1 = GameObject.Find("OscarOverlay1");
+        oOverlay1.SetActive(false);
+        bOverlay1 = GameObject.Find("BertOverlay1");
+        bOverlay1.SetActive(false);
+        dOverlay1 = GameObject.Find("DraculaOverlay1");
+        dOverlay1.SetActive(false);
+        bbOverlay2 = GameObject.Find("BigBirdOverlay2");
+        bbOverlay2.SetActive(false);
+        oOverlay2 = GameObject.Find("OscarOverlay2");
+        oOverlay2.SetActive(false);
+        bOverlay2 = GameObject.Find("BertOverlay2");
+        bOverlay2.SetActive(false);
+        dOverlay2 = GameObject.Find("DraculaOverlay2");
+        dOverlay2.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -157,39 +176,43 @@ public class GameManager : MonoBehaviour
         switch(selected)
         {
             case 0:
-                if(playerOneCurrency >= birdCost)
+                if(playerOneCurrency >= birdCost && canSpawnBigBird1)
                 {
                     playerOneCurrency -= birdCost;
                     GameObject temp = Instantiate(bigBird, new Vector3(-9, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP1";
                     temp.GetComponent<MonsterController>().movingRight = true;
+                    StartCoroutine(cooldownControl(bbOverlay1, 1, 0, 2.5f));
                 }
                 break;
             case 1:
-                if (playerOneCurrency >= oscarCost)
+                if (playerOneCurrency >= oscarCost && canSpawnOscar1)
                 {
                     playerOneCurrency -= oscarCost;
                     GameObject temp = Instantiate(oscar, new Vector3(-9, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP1";
                     temp.GetComponent<MonsterController>().movingRight = true;
+                    StartCoroutine(cooldownControl(oOverlay1, 1, 1, 1.0f));
                 }
                 break;
             case 2:
-                if(playerOneCurrency >= bertCost)
+                if(playerOneCurrency >= bertCost && canSpawnBert1)
                 {
                     playerOneCurrency -= bertCost;
                     GameObject temp = Instantiate(bert, new Vector3(-9, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP1";
                     temp.GetComponent<MonsterController>().movingRight = true;
+                    StartCoroutine(cooldownControl(bOverlay1, 1, 2, 1.75f));
                 }
                 break;
             case 3:
-                if (playerOneCurrency >= draculaCost)
+                if (playerOneCurrency >= draculaCost && canSpawnDracula1)
                 {
                     playerOneCurrency -= draculaCost;
                     GameObject temp = Instantiate(dracula, new Vector3(-9, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP1";
                     temp.GetComponent<MonsterController>().movingRight = true;
+                    StartCoroutine(cooldownControl(dOverlay1, 1, 3, 0.5f));
                 }
                 break;
             case 4:
@@ -202,43 +225,47 @@ public class GameManager : MonoBehaviour
         switch (selected)
         {
             case 0:
-                if (playerTwoCurrency >= birdCost)
+                if (playerTwoCurrency >= birdCost && canSpawnBigBird2)
                 {
                     playerTwoCurrency -= birdCost;
                     GameObject temp = Instantiate(bigBird, new Vector3(4, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP2";
                     temp.GetComponent<MonsterController>().movingRight = false;
                     temp.GetComponent<SpriteRenderer>().flipX = true;
+                    StartCoroutine(cooldownControl(bbOverlay2, 2, 0, 2.5f));
                 }
                 break;
             case 1:
-                if (playerTwoCurrency >= oscarCost)
+                if (playerTwoCurrency >= oscarCost && canSpawnOscar2)
                 {
                     playerTwoCurrency -= oscarCost;
                     GameObject temp = Instantiate(oscar, new Vector3(4, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP2";
                     temp.GetComponent<MonsterController>().movingRight = false;
                     temp.GetComponent<SpriteRenderer>().flipX = true;
+                    StartCoroutine(cooldownControl(oOverlay2, 2, 1, 1.0f));
                 }
                 break;
             case 2:
-                if (playerTwoCurrency >= bertCost)
+                if (playerTwoCurrency >= bertCost && canSpawnBert2)
                 {
                     playerTwoCurrency -= bertCost;
                     GameObject temp = Instantiate(bert, new Vector3(4, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP2";
                     temp.GetComponent<MonsterController>().movingRight = false;
                     temp.GetComponent<SpriteRenderer>().flipX = true;
+                    StartCoroutine(cooldownControl(bOverlay2, 2, 2, 1.75f));
                 }
                 break;
             case 3:
-                if (playerTwoCurrency >= draculaCost)
+                if (playerTwoCurrency >= draculaCost && canSpawnDracula2)
                 {
                     playerTwoCurrency -= draculaCost;
                     GameObject temp = Instantiate(dracula, new Vector3(4, -3, 0), Quaternion.identity);
                     temp.tag = "MonsterP2";
                     temp.GetComponent<MonsterController>().movingRight = false;
                     temp.GetComponent<SpriteRenderer>().flipX = true;
+                    StartCoroutine(cooldownControl(dOverlay2, 2, 3, 0.5f));
                 }
                 break;
             case 4:
@@ -322,6 +349,87 @@ public class GameManager : MonoBehaviour
                     p2Highlight.GetComponent<RectTransform>().anchoredPosition = new Vector2(p2Highlight.GetComponent<RectTransform>().anchoredPosition.x + 75, -2.4f);
                     p2Highlighted++;
                 }
+            }
+        }
+
+    }
+    IEnumerator cooldownControl(GameObject cardOverlay,int player,int creature,float cooldown)
+    {
+        if(player == 1)
+        {
+            cardOverlay.SetActive(true);
+            switch (creature)
+            {
+                case 0:
+                    canSpawnBigBird1 = false;
+                    break;
+                case 1:
+                    canSpawnOscar1 = false;
+                    break;
+                case 2:
+                    canSpawnBert1 = false;
+                    break;
+                case 3:
+                    canSpawnDracula1 = false;
+                    break;
+            }
+        }
+        else
+        {
+            cardOverlay.SetActive(true);
+            switch (creature)
+            {
+                case 0:
+                    canSpawnBigBird2 = false;
+                    break;
+                case 1:
+                    canSpawnOscar2 = false;
+                    break;
+                case 2:
+                    canSpawnBert2 = false;
+                    break;
+                case 3:
+                    canSpawnDracula2 = false;
+                    break;
+            }
+        }
+        yield return new WaitForSeconds(cooldown);
+        if (player == 1)
+        {
+            cardOverlay.SetActive(false);
+            switch (creature)
+            {
+                case 0:
+                    canSpawnBigBird1 = true;
+                    break;
+                case 1:
+                    canSpawnOscar1 = true;
+                    break;
+                case 2:
+                    canSpawnBert1 = true;
+                    break;
+                case 3:
+                    canSpawnDracula1 = true;
+                    break;
+            }
+        }
+        else
+        {
+            cardOverlay.SetActive(false);
+            switch (creature)
+            {
+                case 0:
+                    canSpawnBigBird2 = true;
+                    break;
+                case 1:
+                    canSpawnOscar2 = true;
+                    break;
+                case 2:
+                    canSpawnBert2 = true;
+                    break;
+                case 3:
+                    canSpawnDracula2 = true;
+                    break;
             }
         }
 
